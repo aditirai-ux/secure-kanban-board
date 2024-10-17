85e363ffb4287afbdce65f73bed658007aa874fe
Aditi Rai <aditi45@gmail.com>
2024-10-16 22:02:44 -0400

    modified index.ts to include authenticateToken method

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -8,5 +8,7 @@ const router = Router();
 router.use('/auth', authRoutes);
 // TODO: Add authentication to the API routes
 router.use('/api', apiRoutes);
+// Use the authenticateToken middleware for the /middleware route
+router.use('/middleware', authenticateToken);
 
 export default router;
