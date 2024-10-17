1711e672cb181c3b7b2343a63deeafa4d87edaad
Aditi Rai <aditi45@gmail.com>
2024-10-16 22:11:10 -0400

    change in routes in index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -7,8 +7,8 @@ const router = Router();
 
 router.use('/auth', authRoutes);
 // TODO: Add authentication to the API routes
-router.use('/api', authenticateToken, apiRoutes);
+router.use('/api', apiRoutes);
 // Use the authenticateToken middleware for the /middleware route
-// router.use('/middleware', authenticateToken);
+router.use('/middleware', authenticateToken);
 
 export default router;
